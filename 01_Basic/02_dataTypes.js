9e97e95c0ebbf430bd8d9f83c5f378ab778c5c35
Chetan Sharma <149317262+Ctnsrma@users.noreply.github.com>
2025-06-17 11:41:13 +0000

    DataTypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,12 @@
+// number =>   2
+// bigint
+// string
+// boolean
+// null 
+// undefined
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)       // Object

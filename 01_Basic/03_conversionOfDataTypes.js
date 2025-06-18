7b7073b371c67a8add6a286188306a0ed346e5df
Chetan Sharma <149317262+Ctnsrma@users.noreply.github.com>
2025-06-18 05:53:01 +0000

    Conversion Of DataType

---
+++ 01_Basic/03_conversionOfDataTypes.js
@@ -0,0 +1,50 @@
+let score = "33"
+// console.log(typeof score)
+// console.log(typeof(score))
+                                                //  "33" => 33
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+let score1 = "33abc"
+// console.log(typeof(score1))
+                                                //  "33abc" => NaN
+// let valueInNumber1 = Number(score1)
+// console.log(valueInNumber1)
+// console.log(typeof(valueInNumber1))
+
+let score2 = null
+// console.log(typeof(score2))
+
+// let valueInNumber2 = Number(score2)
+// console.log(valueInNumber2)
+// console.log(typeof(valueInNumber2))
+
+let score3 = undefined
+// console.log(typeof(score3))
+
+// let valueInNumber3 = Number(score3)
+// console.log(valueInNumber3)
+// console.log(typeof(valueInNumber3))
+
+let score4 = true
+// console.log(typeof(score4))
+
+// let valueInNumber4 = Number(score4)
+// console.log(valueInNumber4)
+// console.log(typeof(valueInNumber4))
+
+let score5 = "Chetan"
+// console.log(typeof(score5))
+
+// let valueInNumber5 = Number(score5)
+// console.log(valueInNumber5)
+// console.log(typeof(valueInNumber5))
+
+// true => 1; false => 0
+let isLoggedIn = 5
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// Any Number except 0 => true; 0 => false
+// Empty String "" => false; "chetan"=> true 
